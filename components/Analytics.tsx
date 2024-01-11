64f7fff1977fe45bbb34644d854e7083089ec2af
Grant Imbo <grant.imbo@gmail.com>
2024-01-11 10:07:54 +0800

    test: send GA4 page view

--- components/Analytics.tsx
+++ components/Analytics.tsx
@@ -6,14 +6,14 @@ import ReactGA from "react-ga4";
 
 export default function Analytics({ data }: any) {
   useEffect(() => {
-    // // Multiple products (previously known as trackers)
-    // ReactGA.initialize("G-40N9DDPQQT");
-    // // Send pageview with a custom path
-    // ReactGA.send({
-    //   hitType: "pageview",
-    //   page: `test.com/${data.slug}`,
-    //   title: data.title,
-    // });
+    // Multiple products (previously known as trackers)
+    ReactGA.initialize("G-40N9DDPQQT");
+    // Send pageview with a custom path
+    ReactGA.send({
+      hitType: "pageview",
+      page: `test.com/${data.slug}`,
+      title: data.title,
+    });
     console.log("send page view");
 
     return () => {};
@@ -21,16 +21,3 @@ export default function Analytics({ data }: any) {
 
   return <></>;
 }
-
-// export function sendDatatoGoogleAnalytics({ data }: any) {
-//   // Multiple products (previously known as trackers)
-//   ReactGA.initialize("G-40N9DDPQQT");
-
-//   // Send pageview with a custom path
-//   ReactGA.send({
-//     hitType: "pageview",
-//     page: `test.com/${data.slug}`,
-//     title: data.title,
-//   });
-//   console.log("render inside useeffect");
-// }
