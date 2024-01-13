72131e8dc80902bca55bba91d342962788982656
Grant Imbo <grant.imbo@gmail.com>
2024-01-13 20:30:14 +0800

    feat: improve animation

--- components/Home/HomeIntro.tsx
+++ components/Home/HomeIntro.tsx
@@ -2,6 +2,7 @@
 
 import homeBg from "@/public/imgs/home-bg-lg.png";
 import { shimmer, toBase64 } from "@/utils/BlurData";
+import AnimateBlock from "@/utils/animateBlock";
 import { motion } from "framer-motion";
 import Image from "next/image";
 
@@ -29,48 +30,30 @@ export default function HomeIntro() {
       </motion.figure>
       <section className="md:flex md:items-center">
         <article className="m-0 mx-auto max-w-[450px] px-8 text-center text-[0.9rem] text-softgray md:text-left  lg:text-[1rem]">
-          <motion.h2
-            className="text-xl font-semibold text-white lg:text-2xl"
-            animate={{ opacity: 1, y: 0 }}
-            initial={{ opacity: 0, y: -20 }}
-            transition={{ delay: 0.2 }}
-          >{`Designer — Developer`}</motion.h2>
-          <motion.p
-            animate={{ opacity: 1, y: 0 }}
-            initial={{ opacity: 0, y: -20 }}
-            transition={{ delay: 0.3 }}
-          >
-            {`In the course of more than 11 years, I have been active in the Tech Industry,
+          <AnimateBlock delay={0.3}>
+            <h2 className="text-xl font-semibold text-white lg:text-2xl">{`Designer — Developer`}</h2>
+          </AnimateBlock>
+
+          <AnimateBlock delay={0.6}>
+            <p>
+              {`In the course of more than 11 years, I have been active in the Tech Industry,
             engaging in various projects that span across both creative and
             technical domains.`}
-          </motion.p>
-          <motion.p
-            animate={{ opacity: 1, y: 0 }}
-            initial={{ opacity: 0, y: -20 }}
-            transition={{ delay: 0.4 }}
-          >
-            {`I have experience as a designer, developer, animator, 3D artist,
+            </p>
+          </AnimateBlock>
+          <AnimateBlock delay={0.8}>
+            <p>{`I have experience as a designer, developer, animator, 3D artist,
             video editor, and other related roles. My skill set is adaptable,
-            and I am capable of adjusting to diverse tasks as needed. `}
-          </motion.p>
-          <motion.p
-            animate={{ opacity: 1, y: 0 }}
-            initial={{ opacity: 0, y: -20 }}
-            transition={{ delay: 0.5 }}
-          >
-            {`I am always up for learning new things and never get satisfied with
-            an average output, always striving for something better and fresh.`}
-          </motion.p>
+            and I am capable of adjusting to diverse tasks as needed. `}</p>
+          </AnimateBlock>
+          <AnimateBlock delay={1.0}>
+            <p>{`I am always up for learning new things and never get satisfied with
+            an average output, always striving for something better and fresh.`}</p>
+          </AnimateBlock>
 
-          <motion.div
-            animate={{ opacity: 1, y: 0 }}
-            initial={{ opacity: 0, y: -20 }}
-            transition={{ delay: 0.6 }}
-          >
-            {/* <Link href="/about" className="button">
+          {/* <Link href="/about" className="button">
               👇 Read More
             </Link> */}
-          </motion.div>
         </article>
       </section>
     </section>
