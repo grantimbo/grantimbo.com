71bb7b6d096b4e451a2b0b23423ed761b714160b
Grant Imbo <grant.imbo@gmail.com>
2026-03-17 02:34:47 +0800

    feature: add paralax effect on homepage

---
+++ src/components/Home/HomeContent.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import { AnimatedPortableText } from "@/src/utils/PortableText";
+import AnimateBlock from "@/src/utils/animateBlock";
+
+interface HomeContentProps {
+  data: {
+    title: string;
+    content: any;
+  };
+}
+
+export default function HomeContent({ data }: HomeContentProps) {
+  const scrollToSection = (id: string) => {
+    const element = document.getElementById(id);
+    if (element) {
+      element.scrollIntoView({
+        behavior: "smooth",
+        block: "start", // Aligns the top of the element to the top of the viewport
+      });
+    }
+  };
+  return (
+    <section className="md:flex md:items-center">
+      <article className="m-0 mx-auto max-w-[450px] px-8 text-center text-[0.9rem] leading-tight md:max-w-[490px] md:text-left lg:text-[1rem] [&_h2]:mb-4 [&_p]:mb-2 [&_p]:text-blue-300/40">
+        <AnimateBlock delay={0.3}>
+          <h2 className="mx-auto max-w-[220px] text-xl font-semibold text-white! md:mx-0 md:max-w-[340px] md:text-3xl">
+            {data?.title}
+          </h2>
+        </AnimateBlock>
+
+        <div className="space-y-4">
+          <AnimateBlock delay={0.4}>
+            <AnimatedPortableText value={data?.content} />
+          </AnimateBlock>
+
+          <div className="flex justify-center space-x-2 md:justify-start">
+            <AnimateBlock delay={0.6}>
+              <button
+                onClick={() => scrollToSection("homeProjects")}
+                className="cursor-pointer rounded-full bg-[#0a2d74] px-6 py-2 text-sm font-medium text-white transition-all duration-300 hover:bg-[#0869ac]"
+              >
+                Projects
+              </button>
+            </AnimateBlock>
+            <AnimateBlock delay={0.8}>
+              <button
+                onClick={() => scrollToSection("experience")}
+                className="cursor-pointer rounded-full border-2 border-[#0a2d74] px-6 py-2 text-sm font-medium text-white transition-all duration-300 hover:border-[#0869ac]"
+              >
+                Experience
+              </button>
+            </AnimateBlock>
+          </div>
+        </div>
+      </article>
+    </section>
+  );
+}
--- src/components/Home/HomeIntro.tsx
+++ src/components/Home/HomeIntro.tsx
@@ -1,9 +1,7 @@
-import homeBg from "@/public/imgs/home-bg-lg.png";
-import { shimmer, toBase64 } from "@/src/utils/BlurData";
-import { AnimatedPortableText } from "@/src/utils/PortableText";
 import AnimateBlock from "@/src/utils/animateBlock";
-import { client, urlFor } from "@/src/utils/sanity";
-import Image from "next/image";
+import { client } from "@/src/utils/sanity";
+import HeroParallax from "./HeroParallax";
+import HomeContent from "./HomeContent";
 
 export default async function HomeIntro() {
   const data = await client.fetch(
@@ -15,33 +13,14 @@ export default async function HomeIntro() {
   );
 
   return (
-    <section className="home mx-auto my-20 mb-20 max-w-7xl items-center md:mb-0 md:grid md:min-h-[60vh] md:grid-cols-[1.2fr_1fr] lg:grid-cols-[1.5fr_1fr]">
-      <figure>
-        <Image
-          alt="Creator Thinker"
-          src={data.hero ? urlFor(data.hero).url() : homeBg}
-          sizes="(max-width: 450px) 50vw, (max-width: 768px) 100vw"
-          placeholder={`data:image/svg+xml;base64,${toBase64(
-            shimmer(1104, 930, "#090e20", "#0f1429"),
-          )}`}
-          quality={100}
-          width={1104}
-          height={930}
-        />
-      </figure>
-      <section className="md:flex md:items-center">
-        <article className="m-0 mx-auto max-w-[450px] px-8 text-center text-[0.9rem] leading-tight md:max-w-[490px] md:text-left lg:text-[1rem] [&_*]:text-blue-300/40 [&_h2]:mb-4 [&_p]:mb-2">
-          <AnimateBlock delay={0.3}>
-            <h2 className="mx-auto max-w-[220px] text-xl font-semibold text-white! md:mx-0 md:max-w-[270px] lg:text-2xl">
-              {data?.title}
-            </h2>
-          </AnimateBlock>
-
-          <div className="space-y-4">
-            <AnimatedPortableText value={data?.content} />
-          </div>
-        </article>
-      </section>
+    <section className="home mx-auto my-14 max-w-7xl items-center md:mb-0 md:grid md:min-h-[80vh] md:grid-cols-[1.2fr_1fr] lg:grid-cols-[1.5fr_1fr]">
+      <AnimateBlock delay={0.1}>
+        <div className="relative">
+          <HeroParallax />
+          <div className="absolute right-[25%] bottom-10 z-[1] h-[60%] w-[50%] rounded-full bg-blue-300/10 blur-[100px] dark:bg-blue-500/30" />
+        </div>
+      </AnimateBlock>
+      <HomeContent data={data} />
     </section>
   );
 }
